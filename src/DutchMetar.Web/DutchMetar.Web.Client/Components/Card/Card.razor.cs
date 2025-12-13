5b7c7225101c30a2a64ad49b4eacf9c12248ae6f
Okan <okanemeni@outlook.com>
2025-12-13 17:26:13 +0100

    Adds basic nav menu and card component

---
+++ src/DutchMetar.Web/DutchMetar.Web.Client/Components/Card/Card.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DutchMetar.Web.Client.Components.Card;
+
+public partial class Card : ComponentBase
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+}
