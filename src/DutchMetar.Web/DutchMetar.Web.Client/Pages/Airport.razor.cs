cec3d64a430544cc814163884609512e88f1d1a1
Okan <okanemeni@outlook.com>
2025-12-13 18:22:46 +0100

    Adds Airport page with placeholders

---
+++ src/DutchMetar.Web/DutchMetar.Web.Client/Pages/Airport.razor.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DutchMetar.Web.Client.Pages;
+
+public partial class Airport : ComponentBase
+{
+    [Parameter]
+    public required string AirportIcao { get; set; }
+}
