49981fb8180b34b598508327fcfa35b36afaff08
Okan <okanemeni@outlook.com>
2025-12-13 18:22:46 +0100

    Adds Airport page with placeholders

---
+++ src/DutchMetar.Web/DutchMetar.Web.Client/Components/Card/CardBigValue.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace DutchMetar.Web.Client.Components.Card;
+
+public partial class CardBigValue : ComponentBase
+{
+    [Parameter]
+    public required string Value { get; set; }
+    
+    [Parameter]
+    public required string Label { get; set; }
+}
