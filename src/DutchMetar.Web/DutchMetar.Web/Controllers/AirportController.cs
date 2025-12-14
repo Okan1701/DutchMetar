ff3c1759fa9be0add3a12afa12ac370ae3068da3
Okan <okanemeni@outlook.com>
2025-12-14 17:30:37 +0100

    Implements data retrieval and click events for navbar

---
+++ src/DutchMetar.Web/DutchMetar.Web/Controllers/AirportController.cs
@@ -0,0 +1,24 @@
+﻿using DutchMetar.Core.Features.AirportSummary.Interfaces;
+using DutchMetar.Web.Mapping;
+using DutchMetar.Web.Shared.Constants;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DutchMetar.Web.Controllers;
+
+[ApiController]
+public class AirportController : ControllerBase
+{
+    private readonly IGetAirportSummariesFeature _getAirportSummariesFeature;
+
+    public AirportController(IGetAirportSummariesFeature getAirportSummariesFeature)
+    {
+        _getAirportSummariesFeature = getAirportSummariesFeature;
+    }
+    
+    [HttpGet(EndpointConstants.AirportSummariesEndpoint)]
+    public async Task<IActionResult> GetAirportSummary()
+    {
+        var airports = await _getAirportSummariesFeature.GetAirportSummariesAsync();
+        return Ok(airports.Select(AirportSummaryMapping.Map));
+    }
+}
