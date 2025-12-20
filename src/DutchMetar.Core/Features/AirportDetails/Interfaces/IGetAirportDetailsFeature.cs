954cadfe5a0ec9b594eb4477ab29543ca08cafcf
Okan <okanemeni@outlook.com>
2025-12-20 16:53:22 +0100

    Implements first functionaility for Airports page

---
+++ src/DutchMetar.Core/Features/AirportDetails/Interfaces/IGetAirportDetailsFeature.cs
@@ -0,0 +1,6 @@
+﻿namespace DutchMetar.Core.Features.AIrportDetails.Interfaces;
+
+public interface IGetAirportDetailsFeature
+{
+    Task<AirportDetails.Models.AirportDetails> GetAirportDetailsAsync(string airportCode, CancellationToken cancellationToken = default);
+}
