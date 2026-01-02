d583d4a3e4569cefe0de6493aa48220ef76a298a
Okan <31687801+Okan1701@users.noreply.github.com>
2026-01-02 16:39:52 +0100

    Adds SyncKnmiMetarFileListFeature (#8)

---
+++ src/DutchMetar.Core/Features/SyncKnmiMetarFileList/Infrastructure/KnmiMetarApiOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace DutchMetar.Core.Features.SyncKnmiMetarFileList.Infrastructure;
+
+public class KnmiMetarApiOptions
+{
+    /// <summary>
+    /// Authorization token used to authenticate with the KNMI API
+    /// </summary>
+    public required string AuthorizationToken { get; set; }
+}
