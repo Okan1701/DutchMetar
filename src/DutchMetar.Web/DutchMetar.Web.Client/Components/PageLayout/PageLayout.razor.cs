bcff09592747b372477c9ebd6fb2f3c8d286dd82
Okan <okanemeni@outlook.com>
2026-02-22 16:28:22 +0100

    More improvements to page layout

--- src/DutchMetar.Web/DutchMetar.Web.Client/Components/PageLayout/PageLayout.razor.cs
+++ src/DutchMetar.Web/DutchMetar.Web.Client/Components/PageLayout/PageLayout.razor.cs
@@ -4,9 +4,14 @@ namespace DutchMetar.Web.Client.Components.PageLayout;
 
 public partial class PageLayout : ComponentBase
 {
-    [Parameter]
-    public required PageStatus Status { get; set; }
+    [Parameter] public required PageStatus Status { get; set; } = PageStatus.Displaying;
     
     [Parameter]
     public required RenderFragment PageContent { get; set; }
+    
+    [Parameter]
+    public required RenderFragment TitleContent { get; set; }
+    
+    [Parameter]
+    public RenderFragment? SubtitleContent { get; set; }
 }
