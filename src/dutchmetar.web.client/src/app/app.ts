83628bf2be4153fa8dbe294dbbf14a244654d9cf
Okan Emeni <okanemeni@outlook.com>
2026-05-15 15:31:15 +0200

    Adds basic layout

--- src/dutchmetar.web.client/src/app/app.ts
+++ src/dutchmetar.web.client/src/app/app.ts
@@ -1,11 +1,22 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListItem, MatListModule, MatNavList } from '@angular/material/list';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [
+        RouterOutlet,
+        MatToolbarModule,
+        MatSidenavModule,
+        MatNavList,
+        MatListModule,
+        MatIconModule,
+    ],
     templateUrl: './app.html',
-    styleUrl: './app.scss'
+    styleUrl: './app.scss',
 })
 export class App {
     protected readonly title = signal('DutchMetar');
