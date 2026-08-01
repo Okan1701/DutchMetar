d4f2ad7c90e2fdd2ab3c4093fdfa0d9ca07e2a21
Okan <okanemeni@outlook.com>
2026-08-01 14:16:51 +0200

    Adds simple collapsable sidenav

--- src/DutchMetar.Web.Client/src/app/app.ts
+++ src/DutchMetar.Web.Client/src/app/app.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, computed, HostListener, OnInit, signal, ViewChild } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
 import { AirportService } from './shared/services/airport-service';
 import { AirportNavList } from './shared/components/airport-nav-list/airport-nav-list';
+import { MatIconButton } from '@angular/material/button';
+
+const MOBILE_WIDTH_BREAKPOINT = 786;
 
 @Component({
     selector: 'app-root',
@@ -17,14 +20,29 @@ import { AirportNavList } from './shared/components/airport-nav-list/airport-nav
         MatIconModule,
         AirportNavList,
         RouterLink,
+        MatIconButton,
     ],
     templateUrl: './app.html',
     styleUrl: './app.scss',
 })
 export class App implements OnInit {
+    @ViewChild(MatSidenav)
+    public matSideNav?: MatSidenav;
+    protected showMobileSideNav = computed(() => this.windowWidth() <= MOBILE_WIDTH_BREAKPOINT);
+    private windowWidth = signal<number>(0);
+
     constructor(private readonly airportService: AirportService) {}
 
     public ngOnInit(): void {
         this.airportService.initializeAirports();
+    }
+
+    @HostListener('window:resize')
+    public onResize() {
+        this.windowWidth.set(window.innerWidth);
+    }
+
+    protected toggleSideNav(): void {
+        this.matSideNav?.toggle();
     }
 }
