f0a7543eb77d945e034aa48d1bc3dd9c597ce3fb
Okan <okanemeni@outlook.com>
2026-08-01 16:51:08 +0200

    Reworks latest weather card and improves responsiveness

--- src/DutchMetar.Web.Client/src/app/features/airport/components/airport-latest-weather/airport-latest-weather.ts
+++ src/DutchMetar.Web.Client/src/app/features/airport/components/airport-latest-weather/airport-latest-weather.ts
@@ -1,17 +1,24 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { Stack } from '../../../../shared/components/stack/stack';
 import { LatestWeatherValueCard } from './latest-weather-value-card/latest-weather-value-card';
 import { AirportCurrentMetar } from '../../../../shared/models/airport-current-metar';
+import { MatTooltip } from '@angular/material/tooltip';
+import { MatIcon } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
     selector: 'app-airport-latest-weather',
-    imports: [MatCardModule, Stack, LatestWeatherValueCard],
+    imports: [MatCardModule, Stack, LatestWeatherValueCard, MatTooltip, MatIcon, MatIconButton],
     templateUrl: './airport-latest-weather.html',
     styleUrl: './airport-latest-weather.scss',
 })
 export class AirportLatestWeather {
     public airportWeather = input.required<AirportCurrentMetar>();
+    private readonly clipboard: Clipboard = inject(Clipboard);
+    private readonly snackBar: MatSnackBar = inject(MatSnackBar);
 
     protected issuedAt = computed(() => {
         let issuedAtDate = new Date(this.airportWeather().issuedAt);
@@ -22,4 +29,10 @@ export class AirportLatestWeather {
 
         return issuedAtDate.toISOString().slice(11, 16);
     });
+
+    protected copyMetarToClipboard(): void {
+        if (this.clipboard.copy(this.airportWeather().rawMetar)) {
+            this.snackBar.open('METAR copied to clipboard!');
+        }
+    }
 }
--- src/DutchMetar.Web.Client/src/app/features/airport/components/airport-latest-weather/latest-weather-value-card/latest-weather-value-card.ts
+++ src/DutchMetar.Web.Client/src/app/features/airport/components/airport-latest-weather/latest-weather-value-card/latest-weather-value-card.ts
@@ -1,12 +1,14 @@
 import { Component, input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
     selector: 'app-latest-weather-value-card',
-    imports: [MatCardModule],
+    imports: [MatCardModule, MatIcon],
     templateUrl: './latest-weather-value-card.html',
     styleUrl: './latest-weather-value-card.scss',
 })
 export class LatestWeatherValueCard {
     public label = input<string>('');
+    public icon = input<string>();
 }
