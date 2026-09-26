1b7b2498d1ba8fe56c1eb58a3d67cacf1923f037
Okan <31687801+Okan1701@users.noreply.github.com>
2026-09-26 18:21:48 +0200

    Adds Sentry SDK (#44)

--- src/DutchMetar.Web.Server/Program.cs
+++ src/DutchMetar.Web.Server/Program.cs
@@ -6,7 +6,20 @@ using DutchMetar.Core.Infrastructure;
 using DutchMetar.Core.Infrastructure.Data;
 using DutchMetar.Web.Server.Constants;
 
+const string sentryDsnConnectionString = "SentryDsn";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseSentry(o =>
+{
+    o.Dsn = builder.Configuration.GetConnectionString(sentryDsnConnectionString);
+    // Set TracesSampleRate to 1.0 to capture 100%
+    // of transactions for tracing.
+    // We recommend adjusting this value in production
+    o.TracesSampleRate = 1.0;
+    // Enable logs to be sent to Sentry
+    o.EnableLogs = true;
+});
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
